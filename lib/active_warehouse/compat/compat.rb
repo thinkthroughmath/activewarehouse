8ebfc1c1d10e14008057131711fe0a6ac7502b9c
Chris Geihsler <cgeihsler@thinkthroughmath.com>
2013-02-07 22:17:15 -0500

    Removing method aliasing that no longer makes sense

--- lib/active_warehouse/compat/compat.rb
+++ lib/active_warehouse/compat/compat.rb
@@ -36,14 +36,3 @@ module ActiveRecord #:nodoc:
     end
   end
 end
-
-class Module #:nodoc:
-  def alias_method_chain(target, feature)
-    # Strip out punctuation on predicates or bang methods since
-    # e.g. target?_without_feature is not a valid method name.
-    aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
-    yield(aliased_target, punctuation) if block_given?
-    alias_method "#{aliased_target}_without_#{feature}#{punctuation}", target
-    alias_method target, "#{aliased_target}_with_#{feature}#{punctuation}"
-  end
-end
